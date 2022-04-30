989f209e686751d08db7865ba22183884268910f
Arellano-Jann <jannrellano@nevada.unr.edu>
2022-04-30 11:55:33 -0700

    added != operator overload

--- headers/Entry.h
+++ headers/Entry.h
@@ -16,11 +16,13 @@ public:
     void setValue(const ValueType& value) { this->value = value; }
     const KeyType& getKey() const { return key; }
     const ValueType& getValue() const { return value; }
-    
+
     bool operator==(const Entry<KeyType, ValueType>& rhs) const; // checks if both entries are the same
     bool operator==(const KeyType& rhs) const; // check if both keys are the same
     bool operator >(const Entry<KeyType, ValueType>& rhs) const; // checks if the entry is greater than rhs entry
     bool operator >(const KeyType& rhs) const; // checks if the key is greater than rhs key
+    bool operator!=(const Entry<KeyType, ValueType>& rhs) const;
+    bool operator!=(const KeyType& rhs) const;
 };
 
 #include "../src/Entry.cpp"
