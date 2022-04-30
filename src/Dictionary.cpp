ac706c28f818d781817b3c05cae16e894534b023
Arellano-Jann <jannrellano@nevada.unr.edu>
2022-04-30 10:28:32 -0700

    Stubbed out

---
+++ src/Dictionary.cpp
@@ -0,0 +1,44 @@
+template <typename KeyType, typename ValueType>
+int Dictionary<KeyType, ValueType>::hash(const KeyType& key) const {
+
+}
+
+template <typename KeyType, typename ValueType>
+bool Dictionary<KeyType, ValueType>::isEmpty() const{
+
+}
+
+template <typename KeyType, typename ValueType>
+int Dictionary<KeyType, ValueType>::getNumberOfEntries() const{
+
+}
+
+template <typename KeyType, typename ValueType>
+bool Dictionary<KeyType, ValueType>::add(const KeyType& newKey, const ValueType& newValue){
+
+}
+
+template <typename KeyType, typename ValueType>
+bool Dictionary<KeyType, ValueType>::remove(const KeyType& key){
+
+}
+
+template <typename KeyType, typename ValueType>
+bool Dictionary<KeyType, ValueType>::clear(){
+
+}
+
+template <typename KeyType, typename ValueType>
+bool Dictionary<KeyType, ValueType>::contains(const KeyType& key) const{
+
+}
+
+template <typename KeyType, typename ValueType>
+ValueType Dictionary<KeyType, ValueType>::getValue(const KeyType& key) const{
+
+}
+
+template <typename KeyType, typename ValueType>
+Dictionary<KeyType, ValueType>::~Dictionary(){
+    
+}
